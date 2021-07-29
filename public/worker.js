630ed6ef5966b72b60e1d94e0cf12a523220aadf
William Watson <williampwatson1991@gmail.com>
2021-07-29 14:00:47 -0400

    feat: added worker.js file/initial commit

---
+++ public/worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'hackathon-rummy';
+
+const urlsToCache = ['/'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', (event) => {
+  var cacheWhitelist = ['hackathon-rummy'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
