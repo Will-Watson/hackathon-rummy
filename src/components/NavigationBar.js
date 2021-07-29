d0926f8c806c7be0c834f27bdecd717237e10520
William Watson <williampwatson1991@gmail.com>
2021-07-29 15:20:15 -0400

    fix: minor changes, spelling

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -24,7 +24,7 @@ const NavigationBar = () => {
         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
         <Navbar.Collapse id='responsive-navbar-nav'>
           <Nav
-            className='ml-auto'
+            className='ml-auto navbar-nav'
             activeKey={selected}
             onSelect={(selectedItem) => setSelected(selectedItem)}
             as='ul'
