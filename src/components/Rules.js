8f1504f244e77382a84a63a78d752db80b331bb0
William Watson <williampwatson1991@gmail.com>
2021-07-30 14:02:15 -0400

    fix: some rules images werent corrent. feat: information regarding rules and its respective images are now mapped to the rules component for clarity

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import RulesImage1 from '../images/RulesImage1.jpeg';
-import RulesImage2 from '../images/RulesImage2.jpeg';
-import RulesImage3 from '../images/RulesImage3.jpeg';
-import RulesImage4 from '../images/RulesImage4.jpeg';
-import RulesImage5 from '../images/RulesImage5.jpeg';
-import RulesImage6 from '../images/RulesImage6.jpeg';
-import RulesImage7 from '../images/RulesImage7.jpeg';
-import RulesImage8 from '../images/RulesImage8.jpeg';
+// import RulesImage1 from '../images/RulesImage1.jpeg';
+// import RulesImage2 from '../images/RulesImage2.jpeg';
+// import RulesImage3 from '../images/RulesImage3.jpeg';
+// import RulesImage4 from '../images/RulesImage4.jpeg';
+// import RulesImage5 from '../images/RulesImage5.jpeg';
+// import RulesImage6 from '../images/RulesImage6.jpeg';
+// import RulesImage7 from '../images/RulesImage7.jpeg';
+// import RulesImage8 from '../images/RulesImage8.jpeg';
+import { rules } from '../data';
 
 const Rules = () => {
   return (
@@ -18,59 +19,13 @@ const Rules = () => {
       </div>
       <div className='container'>
         <div className='row my-5'>
-          <div className='col-md-4 my-4'>
-            <img src={RulesImage1} alt='cards' className='w-100' />
-            <h4 className='my-4'>Rule #1</h4>
-            <p>this is where the rule explanation is going to go</p>
-          </div>
-
-          <div className='col-md-4 my-4'>
-            <img src={RulesImage2} alt='cards' className='w-100' />
-            <h4 className='my-4'>Rule #1</h4>
-            <p>this is where the rule explanation is going to go</p>
-          </div>
-
-          <div className='col-md-4 my-4'>
-            <img src={RulesImage3} alt='cards' className='w-100' />
-            <h4 className='my-4'>Rule #1</h4>
-            <p>this is where the rule explanation is going to go</p>
-          </div>
-
-          <div className='col-md-4 my-4'>
-            <img src={RulesImage4} alt='cards' className='w-100' />
-            <h4 className='my-4'>Rule #1</h4>
-            <p>this is where the rule explanation is going to go</p>
-          </div>
-
-          <div className='col-md-4 my-4'>
-            <img src={RulesImage5} alt='cards' className='w-100' />
-            <h4 className='my-4'>Rule #1</h4>
-            <p>this is where the rule explanation is going to go</p>
-          </div>
-
-          <div className='col-md-4 my-4'>
-            <img src={RulesImage6} alt='cards' className='w-100' />
-            <h4 className='my-4'>Rule #1</h4>
-            <p>this is where the rule explanation is going to go</p>
-          </div>
-
-          <div className='col-md-4 my-4'>
-            <img src={RulesImage7} alt='cards' className='w-100' />
-            <h4 className='my-4'>Rule #1</h4>
-            <p>this is where the rule explanation is going to go</p>
-          </div>
-
-          <div className='col-md-4 my-4'>
-            <img src={RulesImage8} alt='cards' className='w-100' />
-            <h4 className='my-4'>Rule #1</h4>
-            <p>this is where the rule explanation is going to go</p>
-          </div>
-
-          <div className='col-md-4 my-4'>
-            <img src={RulesImage8} alt='cards' className='w-100' />
-            <h4 className='my-4'>Rule #1</h4>
-            <p>this is where the rule explanation is going to go</p>
-          </div>
+          {rules.map((rule) => (
+            <div className='col-md-4 my-4'>
+              <img src={rule.img} alt='Rule' className='w-100' />
+              <h4 className='my-4'>{rule.title}</h4>
+              <p>{rule.rule}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/data.js
@@ -0,0 +1,56 @@
+import RulesImage1 from './images/RulesImage1.jpeg';
+import RulesImage2 from './images/RulesImage2.jpeg';
+import RulesImage3 from './images/RulesImage3.jpeg';
+import RulesImage4 from './images/RulesImage4.jpeg';
+import RulesImage5 from './images/RulesImage5.jpeg';
+import RulesImage6 from './images/RulesImage6.jpeg';
+import RulesImage7 from './images/RulesImage7.jpeg';
+import RulesImage8 from './images/RulesImage8.jpeg';
+
+export const rules = [
+  {
+    img: RulesImage8,
+    title: 'RUMMY!',
+    rule: 'Rummy is achieved when one player has one of the following in their hand. \n three of a kind and four of a kind \n three of a kind and a suited four card run \n four of a kind and a suited three card run. Pictured above are some examples.',
+  },
+  {
+    img: RulesImage1,
+    title: 'First',
+    rule: 'Both players are dealt seven cards, one at a time to each player, face down.',
+  },
+  {
+    img: RulesImage2,
+    title: 'Second',
+    rule: 'The remainder of the deck is then placed face down between both players with the top card turned over. The face up card will be the start of the discard pile.',
+  },
+  {
+    img: RulesImage3,
+    title: 'Third',
+    rule: 'Each player will look at their hand. At this time the player who goes first will decide whether or not the face up card will benefit their hand. Here we can see that the 5 of Hearts does indeed benefit this hand.',
+  },
+  {
+    img: RulesImage4,
+    title: 'Fourth',
+    rule: 'The player will then take the card from the discard pile. (Important to note that only the top card can be drawn from the discard pile.) Because each player can only have seven cards in their respective hands at any time, the player is forced to discard one.',
+  },
+  {
+    img: RulesImage5,
+    title: 'Fifth',
+    rule: "We can see here that the 6 of Clubs was discarded. Now its the second players turn. The 6 of Clubs won't benefit this players hand, so they can now draw from the top of the deck. (The player does not have to show this card to the opposing player.)",
+  },
+  {
+    img: RulesImage6,
+    title: 'Sixth',
+    rule: 'The player has drawn the 9 of Spades. This card also does not benefit the hand.',
+  },
+  {
+    img: RulesImage7,
+    title: 'Seventh',
+    rule: 'The player then has to discard one card from their hand. Here we can see that 9 of spades was discarded as its the least beneficial card in the course to achieve Rummy.',
+  },
+  {
+    img: RulesImage8,
+    title: 'Lastly',
+    rule: 'Each player will continue these steps during their turns until one player achieves Rummy.',
+  },
+];
