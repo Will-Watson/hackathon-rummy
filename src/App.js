4ca043840d50261239c897f2b0396a0236f6de2a
William Watson <williampwatson1991@gmail.com>
2021-07-30 16:20:00 -0400

    feat: footer component skeleton created and added to app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import Game from './components/Game';
 
@@ -13,6 +14,7 @@ function App() {
         <NavigationBar />
         <Route exact path='/' component={Home} />
         <Route exact path='/hackathon-rummy-game' component={Game} />
+        <Footer />
       </BrowserRouter>
     </div>
   );
