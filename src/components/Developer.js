ad587afa473aa00f10cfc092a79bb6b783f0cb2c
William Watson <williampwatson1991@gmail.com>
2021-07-30 16:14:17 -0400

    feat: developer card complete for landing page

--- src/components/Developer.js
+++ src/components/Developer.js
@@ -2,10 +2,19 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import DeveloperCardImage from '../images/DeveloperCardImage.jpeg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTwitter,
+  faGithub,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
 
 const name = 'William Watson';
 const bio =
   'Software Engineer from Queens, New York with a passion for front-end mobile and web application development. When William is not coding you might find him working on his golf game or rewatching the X-Files.';
+const twitterIcon = <FontAwesomeIcon icon={faTwitter} size='2x' />;
+const githubIcon = <FontAwesomeIcon icon={faGithub} size='2x' />;
+const linkedinIcon = <FontAwesomeIcon icon={faLinkedin} size='2x' />;
 
 const Developer = () => {
   return (
@@ -17,6 +26,29 @@ const Developer = () => {
         <Card.Body className='card-body'>
           <Card.Title className='card-title'>{name}</Card.Title>
           <Card.Text>{bio}</Card.Text>
+          <div className='socials'>
+            <a
+              href='https://twitter.com/concatWill'
+              target='_blank'
+              rel='noreferrer'
+            >
+              {twitterIcon}
+            </a>
+            <a
+              href='https://www.linkedin.com/in/williampwatson'
+              target='_blank'
+              rel='noreferrer'
+            >
+              {linkedinIcon}
+            </a>
+            <a
+              href='https://github.com/Will-Watson'
+              target='_blank'
+              rel='noreferrer'
+            >
+              {githubIcon}
+            </a>
+          </div>
         </Card.Body>
       </Card>
     </Container>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Rules from '../components/Rules';
 import Developer from '../components/Developer';
 
