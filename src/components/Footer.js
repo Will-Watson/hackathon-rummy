6645c79bf300e88f82769a873545e840448b97dd
William Watson <williampwatson1991@gmail.com>
2021-07-30 16:34:42 -0400

    feat: dummy footer added and minor changes to dev card

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div>
+      <footer>
+        <div className='container col-sm-10 col-md-8 col-lg-12'>
+          <div className='row text-dark text-center py-4 justify-content-center'>
+            <div className='col-sm-10 col-md-8 col-lg-6'>
+              <p>All inquiries please email williampwatson1991@gmail.com</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div className='socket text-center py-3'>
+        <a
+          href='https://github.com/will-watson'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Designed and Developed by William Watson.
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
