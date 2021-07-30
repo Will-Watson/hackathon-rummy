985f3261c5b9b01a5694fa131e80e054bd9b0e66
William Watson <williampwatson1991@gmail.com>
2021-07-30 14:04:48 -0400

    feat: rules/instructions section complete. for now.

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,12 +1,4 @@
 import React from 'react';
-// import RulesImage1 from '../images/RulesImage1.jpeg';
-// import RulesImage2 from '../images/RulesImage2.jpeg';
-// import RulesImage3 from '../images/RulesImage3.jpeg';
-// import RulesImage4 from '../images/RulesImage4.jpeg';
-// import RulesImage5 from '../images/RulesImage5.jpeg';
-// import RulesImage6 from '../images/RulesImage6.jpeg';
-// import RulesImage7 from '../images/RulesImage7.jpeg';
-// import RulesImage8 from '../images/RulesImage8.jpeg';
 import { rules } from '../data';
 
 const Rules = () => {
@@ -15,7 +7,9 @@ const Rules = () => {
       <div className='col-12 text-center mt-5'>
         <h1 className='text-dark pt-4'>Instructions</h1>
         <div className='border-top border-primary w-25 mx-auto my-3'></div>
-        <p className='lead'>something should be here</p>
+        <p className='lead'>
+          The object of the game is to be the first to achieve Rummy.
+        </p>
       </div>
       <div className='container'>
         <div className='row my-5'>
