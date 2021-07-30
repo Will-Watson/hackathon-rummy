8056a9b9cb6f9832ad4b2404d5bdd55d8587e6e6
William Watson <williampwatson1991@gmail.com>
2021-07-30 14:16:10 -0400

    feat: bare bones developer skeleton component created and added to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Rules from '../components/Rules';
+import Developer from '../components/Developer';
 
 const Home = () => {
   return (
     <React.Fragment>
       <Rules />
+      <Developer />
     </React.Fragment>
   );
 };
