0636e2e5c934bbf8729c40f77244e4c783b40c76
William Watson <williampwatson1991@gmail.com>
2021-07-30 17:22:33 -0400

    feat: store should be connected

--- src/store/game.js
+++ src/store/game.js
@@ -1,13 +1,46 @@
 import axios from 'axios';
 
-const initialState = {};
+const initialState = {
+  deck: {},
+};
 
 //action types
+const SHUFFLE = 'SHUFFLE';
 
 //action creators
+export const shuffle = (deck) => {
+  return {
+    type: SHUFFLE,
+    deck,
+  };
+};
 
 //thunks
-
+export const shuffleDeck = () => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(
+        'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+      );
+      console.log(data);
+      dispatch(shuffle(data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
 //reducer
 
-export default (state = intialState, action) => {};
+const game = (state = initialState, action) => {
+  switch (action.type) {
+    case SHUFFLE:
+      return {
+        ...state,
+        deck: action.deck,
+      };
+    default:
+      return state;
+  }
+};
+
+export default game;
